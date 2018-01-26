df66043ba17f9c913f973885ec3954e2b295fcc0
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-01-25 22:04:35 -0600

    Add files via upload

---
+++ src/assignments/main_assignment2.py
@@ -0,0 +1,6 @@
+from assignment2 import faculty_evaluation_result
+
+'''Write code to call the faculty_evaluation_result function with data of your choice'''
+print(faculty_evaluation_result (80, 30, 50, 70, 80, 90))
+print(faculty_evaluation_result (80, 80, 50, 80, 80, 90))
+print(faculty_evaluation_result (0, 30, 50, 70, 80, 90))
