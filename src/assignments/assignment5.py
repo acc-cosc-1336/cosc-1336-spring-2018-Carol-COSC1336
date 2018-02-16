50c535ec437510897b4f03c30af1005ae556eb0a
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-02-15 21:35:10 -0600

    Add files via upload

---
+++ src/assignments/assignment5.py
@@ -0,0 +1,16 @@
+def recursive_decimal_binary(num, bits):
+    '''
+    Given a whole number from the decimal system, converts the number to 8-bit binary
+    Given 86 returns 01010110
+    :param num:
+    :return:
+    '''
+    #bits are 0,1,2,3,4,5,6,7
+    power_value = 2**bits  # power values 128,64,32,6,8,4,2,1
+    if num == 0 and bits < 0:
+        return ''
+    elif num >= power_value:
+        return '1' + recursive_decimal_binary(num-power_value, bits-1)
+        
+    else:
+        return '0' + recursive_decimal_binary(num, bits-1)
---
+++ src/assignments/main_assignment5.py
@@ -0,0 +1,21 @@
+from assignment5 import recursive_decimal_binary
+#write import statement for recursive_decimal_binary
+
+'''
+Write a for loop from 1 to 255 and call the recursive_decimal_binary function with loop index/target variable
+as an argument and print the decimal and binary value next to each other as follows:
+1 00000001
+2 00000010
+3 00000011
+4 00000100
+5 00000101
+6 00000110
+7 00000111
+etc
+'''
+def main():
+    bits = 7
+    for index in range (1,256):
+        result = recursive_decimal_binary(index, bits)
+        print(index,result)
+main()
