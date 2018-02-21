3dcb348bb5f56ce63b37fe7b1ea9fbbc152d749b
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-02-21 16:07:49 -0600

    Add files via upload

---
+++ src/homework/homework5.py
@@ -0,0 +1,17 @@
+#Create a function named write_sales_data with file_object, item and price as parameters.
+#The function should write item and price to a file.
+
+def write_sales_data (file_object, item, price):
+    file_object.write (str(item)+ '\t'+'\t' + str(price)+ "\n")
+    
+
+#Create another function named read_sales_data with file_object as a parameter.
+#The function will read the file line by line and display to screen to produce the table described in homework 5.
+
+def read_sales_data(file_object):
+    
+    line = file_object.readline()
+    print(line)
+    while line!= '':
+        line = file_object.readline()
+        print(line)
