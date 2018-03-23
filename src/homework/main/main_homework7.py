82b32e6e5efb33e4462113c747d4a294a640cd2f
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-03-23 16:28:12 -0500

    Add files via upload

---
+++ src/homework/main/main_homework7.py
@@ -0,0 +1,41 @@
+#write import statement for homework 7 file
+from src.homework.homework7 import get_p_distance_matrix
+'''
+Write a main function to...
+Read p_distance.dat file
+From the file data, create a two-dimensional list like the following example:
+
+[
+ ['T','T','T','C','C','A','T','T','T','A'],
+ ['G','A','T','T','C','A','T','T','T','C'],
+ ['T','T','T','C','C','A','T','T','T','T'],
+ ['G','T','T','C','C','A','T','T','T','A']
+]
+
+Pass the list to the get_p_distance_matrix function as an argument
+Display the p distance matrix to screen
+'''
+
+def main():
+
+    file = open('p_distance.dat', 'r')
+    return_list = []
+    list1 = []
+    p_distance = []
+    for line in file:
+        list1 = line.split()
+        return_list.append(list1)
+    print (return_list)
+    
+    p_distance = get_p_distance_matrix(return_list)
+
+    file.close()
+    print(p_distance)
+    return p_distance
+
+main()
+
+    
+
+            
+        
