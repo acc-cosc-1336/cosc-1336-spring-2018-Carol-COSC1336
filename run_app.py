da90b879ad72bde78a40cef915be69df4c73624a
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-03-28 17:34:22 -0500

    Add files via upload

--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
-import src.homework.main.main_homework7
-#import main_homework6
-#running this file will run the code in main_homework6.py file without the need to modify
+import src.homework.main.main_homework8
+
+#running this file will run the code in main_homework8.py file without the need to modify
 #code in that file
