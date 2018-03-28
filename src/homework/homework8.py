1b8975f96dc4d54d63b8ad55a09d896fc512a15d
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-03-28 17:33:41 -0500

    Add files via upload

--- src/homework/homework8.py
+++ src/homework/homework8.py
@@ -9,17 +9,16 @@ If the widget exists it will update the quantity of the widgets.
     :param quantity:    Running count of inventory on hand
     :return:
 '''
+
 def add_inventory(widgets, widget_name, quantity):
 
     if widget_name not in widgets:
         widgets[widget_name] = quantity
-        print('1st if - NOT IN', widgets)
     else:
-        count = widgets[widget_name]
-        quantity += count
-        print('2nd if - IN', widgets)
-  
-    return quantity
+        widgets[widget_name] += quantity
+        
+    return widgets
+
             
 
 '''
@@ -32,13 +31,10 @@ Otherwise it returns 'Item not found'
     :param widget_name:
     :return:
 '''
-def remove_inventory_widget(widget_name):
-
-    widgets={}
+def remove_inventory_widget(widgets, widget_name):
 
     if widget_name in widgets:
-        del widgets['widget_name']
-        print('Record Deleted')
+        del widgets[widget_name]
+        return('Record deleted')
     else:
-        if widget_name not in widgets:
-            print('Item not found')
+        return('Item not found')
