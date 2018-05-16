56328de0f2a33c89172f8182e4b10801c65755e0
Carol-COSC1336 <35587133+Carol-COSC1336@users.noreply.github.com>
2018-05-16 00:38:05 -0500

    Add files via upload

--- src/final/motif.py
+++ src/final/motif.py
@@ -1,1 +1,11 @@
 
+def non_contiguous_motif(str1, dna_list):
+    dna_list = "".join(str(x) for x in dna_list)
+    index=0
+    position = 0
+    position_list = []
+    for index in range (0, len(str1)):
+        position = dna_list.find(str1[index])
+        position_list.append(position+1)
+        index +=1
+    return tuple(position_list)
